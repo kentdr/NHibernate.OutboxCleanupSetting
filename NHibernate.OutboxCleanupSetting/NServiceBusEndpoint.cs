871f56a6d32f37e906c69ab2994f48ae10b4c969
Dan Kent <83468000+kentdr@users.noreply.github.com>
2021-08-24 09:42:05 -0400

    Initial Commit

---
+++ NHibernate.OutboxCleanupSetting/NServiceBusEndpoint.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Hosting;
+
+using NServiceBus;
+using NServiceBus.Persistence;
+
+using System.Configuration;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NHibernate.OutboxCleanupSetting
+{
+    internal class NServiceBusEndpoint : IHostedService
+    {
+        private IEndpointInstance _endpoint;
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            var endpointConfiguration = new EndpointConfiguration("no-cleanup-setting");
+            endpointConfiguration.SetDiagnosticsPath(Directory.GetCurrentDirectory());
+            endpointConfiguration.UseTransport<LearningTransport>();
+            endpointConfiguration.EnableOutbox();
+            var persistence = endpointConfiguration.UsePersistence<NHibernatePersistence>();
+            persistence.ConnectionString(@"Data Source=(LocalDb)\MSSQLLocalDB;Integrated Security=true");
+            System.Diagnostics.Debug.WriteLine($"Test: {ConfigurationManager.AppSettings.Get("Test")}");
+            _endpoint = await Endpoint.Start(endpointConfiguration);
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await _endpoint.Stop();
+        }
+    }
+}
